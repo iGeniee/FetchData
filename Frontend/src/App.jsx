d774a2d90dd69bba24a688d34ac6e57b49efdfaf
iGeniee <146809581+iGeniee@users.noreply.github.com>
2024-05-12 15:24:19 +0800

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React, {useState,useEffect} from 'react'
+
+
+function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('')
+    .then(res => res.json())
+    .then (data => setData(data))
+    .catch(err => console.log(err));
+  }, [])
+  return (
+    <div>
+       <table>
+        <thead>
+          <th>Email</th>
+          <th>Subject</th>
+          <th>Message</th>
+        </thead>
+        <tbody>
+          {data.map((d,i)=> (
+            <tr key={i}>
+              <td>{d.email}</td>
+              <td>{d.subject}</td>
+              <td>{d.message}</td>
+            </tr>
+          ))}
+        </tbody>
+       </table>
+    </div>
+  )
+}
+
+export default App
